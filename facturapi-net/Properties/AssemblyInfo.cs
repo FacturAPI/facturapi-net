7e73475dfa995c53a65bc7b8b73a022764a3060b
Javier Rosas <javier.rosasb@gmail.com>
2017-06-12 01:32:31 -0700

    change description

--- facturapi-net/Properties/AssemblyInfo.cs
+++ facturapi-net/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Facturapi")]
-[assembly: AssemblyDescription("Crea Facturas Electrónicas válidas en México")]
+[assembly: AssemblyDescription("Crea Facturas Electrónicas válidas en México lo más fácil posible (CFDI)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Facturapi")]
 [assembly: AssemblyProduct("Facturapi")]
