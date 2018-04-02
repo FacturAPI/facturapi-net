a775ae93fcfab381a61a4fb27b180a032583a606
Javier Rosas <javier.rosasb@gmail.com>
2018-04-02 03:43:57 -0700

    add constants files

---
+++ facturapi-net/Constants/InvoiceUse.cs
@@ -0,0 +1,28 @@
+﻿namespace Facturapi
+{
+    public static class InvoiceUse
+    {
+        public const string ADQUISICION_MERCANCIAS = "G01";
+        public const string DEVOLUCIONES_DESCUENTOS_BONIFICACIONES = "G02";
+        public const string GASTOS_EN_GENERAL = "G03";
+        public const string CONSTRUCCIONES = "I01";
+        public const string MOBILIARIO_Y_EQUIPO_DE_OFICINA = "I02";
+        public const string EQUIPO_DE_TRANSPORTE = "I03";
+        public const string EQUIPO_DE_COMPUTO = "I04";
+        public const string DADOS_TROQUELES_HERRAMENTAL = "I05";
+        public const string COMUNICACIONES_TELEFONICAS = "I06";
+        public const string COMUNICACIONES_SATELITALES = "I07";
+        public const string OTRA_MAQUINARIA = "I08";
+        public const string HONORARIOS_MEDICOS = "D01";
+        public const string GASTOS_MEDICOS_POR_INCAPACIDAD = "D02";
+        public const string GASTOS_FUNERALES = "D03";
+        public const string DONATIVOS = "D04";
+        public const string INTERESES_POR_CREDITOS_HIPOTECARIOS = "D05";
+        public const string APORTACIONES_VOLUNTARIAS_SAR = "D06";
+        public const string PRIMA_SEGUROS_GASTOS_MEDICOS = "D07";
+        public const string GASTOS_TRANSPORTACION_ESCOLAR = "D08";
+        public const string CUENTAS_AHORRO_PENSIONES = "D09";
+        public const string SERVICIOS_EDUCATIVOS = "D10";
+        public const string POR_DEFINIR = "P01";
+    }
+}
---
+++ facturapi-net/Constants/PaymentMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace Facturapi
+{
+    public static class PaymentMethod
+    {
+        public const string PAGO_EN_UNA_SOLA_EXHIBICION = "PUE";
+        public const string PAGO_EN_PARCIALIDADES_O_DIFERIDO = "PPD";
+    }
+}
