392bc247e993db277f10aff164c07cb57d7e1379
Javier Rosas <javier.rosasb@gmail.com>
2018-05-12 09:39:19 -0700

    remove code that would be deprecated on arrival

--- facturapi-net/Models/Organization.cs
+++ facturapi-net/Models/Organization.cs
@@ -1,8 +1,5 @@
-using Facturapi.Wrappers;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace Facturapi
 {
@@ -15,35 +12,5 @@ namespace Facturapi
         public Legal Legal { get; set; }
         public Customization Customization { get; set; }
         public Certificate Certificate { get; set; }
-
-        public static Task<SearchResult<Organization>> ListAsync (Dictionary<string, object> query = null)
-        {
-            return new OrganizationWrapper().ListAsync(query);
-        }
-
-        public static Task<Organization> CreateAsync (Dictionary<string, object> data)
-        {
-            return new OrganizationWrapper().CreateAsync(data);
-        }
-
-        public static Task<Organization> RetrieveAsync (string id)
-        {
-            return new OrganizationWrapper().RetrieveAsync(id);
-        }
-
-        public static Task<Organization> DeleteAsync (string id)
-        {
-            return new OrganizationWrapper().DeleteAsync(id);
-        }
-
-        public static Task<Organization> UploadLogoAsync (string id, Stream file)
-        {
-            return new OrganizationWrapper().UploadLogoAsync(id, file);
-        }
-        
-        public static Task<Organization> UploadCertificateAsync (string id, Stream cerFile, Stream keyFile, string password)
-        {
-            return new OrganizationWrapper().UploadCertificateAsync(id, cerFile, keyFile, password);
-        }
     }
 }
