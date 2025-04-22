792b69cd07e1dd70b73218e3d8bb63fb4950021e
javorosas <javier.rosasb@gmail.com>
2025-04-22 10:43:29 +0200

    add missing webhook events

--- Constants/WebhookEvents.cs
+++ Constants/WebhookEvents.cs
@@ -5,8 +5,10 @@ namespace Facturapi
         public const string INVOICE_CREATED = "invoice.global_invoice_created";
         public const string STATUS_UPDATED = "invoice.status_updated";
         public const string CANCELLATION_STATUS_UPDATED = "invoice.cancellation_status_updated";
+        public const string GLOBAL_INVOICE_CREATED = "invoice.global_invoice_created";
+        public const string INVOICES_CREATED_FROM_DASHBOARD = "invoice.created_from_dashboard";
         public const string SELF_INVOICE_COMPLETE = "receipt.self_invoice_complete";
         public const string RECEIPT_STATUS_UPDATED = "receipt.status_updated";
-        public const string RECEIPT_CANCELLATION_STATUS_UPDATED = "receipt.cancellation_status_updated";
+        public const string CUSTOMER_EDIT_LINK_COMPLETED = "customer.edit_link_completed";
     }
 }
