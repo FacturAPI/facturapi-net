d81d83a140eedffa557a1a950c111e37f336f71a
javorosas <javier.rosasb@gmail.com>
2025-12-10 09:41:12 -0700

    refactor(CatalogWrapper): streamline search methods by introducing SearchCatalogAsync for error handling and response parsing

--- Wrappers/CatalogWrapper.cs
+++ Wrappers/CatalogWrapper.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Facturapi.Wrappers
@@ -14,173 +13,74 @@ namespace Facturapi.Wrappers
 
         public async Task<SearchResult<CatalogItem>> SearchProducts(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchProductKeys(query));
-            await this.ThrowIfErrorAsync(response);
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchProductKeys(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchUnits(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchUnitKeys(query));
-            await this.ThrowIfErrorAsync(response);
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchUnitKeys(query));
         }
 
         // Carta Porte catalogs
         public async Task<SearchResult<CatalogItem>> SearchAirTransportCodes(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteAirTransportCodes(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteAirTransportCodes(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchTransportConfigs(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteTransportConfigs(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteTransportConfigs(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchRightsOfPassage(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteRightsOfPassage(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteRightsOfPassage(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchCustomsDocuments(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteCustomsDocuments(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteCustomsDocuments(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchPackagingTypes(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaportePackagingTypes(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaportePackagingTypes(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchTrailerTypes(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteTrailerTypes(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteTrailerTypes(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchHazardousMaterials(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteHazardousMaterials(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteHazardousMaterials(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchNavalAuthorizations(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteNavalAuthorizations(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaporteNavalAuthorizations(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchPortStations(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaportePortStations(query));
-            var resultString = await response.Content.ReadAsStringAsync();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
-            }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
+            return await this.SearchCatalogAsync(Router.SearchCartaportePortStations(query));
         }
 
         public async Task<SearchResult<CatalogItem>> SearchMarineContainers(Dictionary<string, object> query = null)
         {
-            var response = await client.GetAsync(Router.SearchCartaporteMarineContainers(query));
-            var resultString = await response.Content.ReadAsStringAsync();
+            return await this.SearchCatalogAsync(Router.SearchCartaporteMarineContainers(query));
+        }
 
-            if (!response.IsSuccessStatusCode)
+        private async Task<SearchResult<CatalogItem>> SearchCatalogAsync(string url)
+        {
+            using (var response = await client.GetAsync(url))
             {
-                var error = JsonConvert.DeserializeObject<JObject>(resultString, this.jsonSettings);
-                throw new FacturapiException(error["message"].ToString());
+                await this.ThrowIfErrorAsync(response);
+                var resultString = await response.Content.ReadAsStringAsync();
+                var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
+                return searchResult;
             }
-
-            var searchResult = JsonConvert.DeserializeObject<SearchResult<CatalogItem>>(resultString, this.jsonSettings);
-            return searchResult;
         }
     }
 }
